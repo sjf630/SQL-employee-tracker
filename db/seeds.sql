9695ad847fd3d34040cbe61b1d0cee3b2e31a9f8
sjf630 <95392688+sjf630@users.noreply.github.com>
2023-07-20 16:43:00 -0500

    fix errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-USE employeeTrackerDB;
+USE employee_db;
 
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES 
 ("Accounting"),
 ("Sales"),
@@ -9,20 +9,20 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-("Accountant", 70000, 1),
+("Accountant", 120000, 1),
 ("Sales Lead", 100000, 2),
 ("Salesperson", 80000, 2),
 ("Lead Engineer", 150000, 3),
-("Software Engineer", 120000, 3),
+("Software Engineer", 100000, 3),
 ("HR Specialist", 65000, 4),
-("HR Assistant", 60000, 4); 
+("HR Assistant", 50000, 4); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ("John", "Doe", 1, null),
 ("Mary", "Smith", 2, null),
 ("Sarah", "Jessica", 2, 2),
-("Matthew", "James", 3, 2),
-("Jesse", "Finn", 4, 3),
-("Amanda", "Thompson", 5, 4),
-("Jason", "Bateman", 7, 6); 
+("Matthew", "Thompson", 3, 2),
+("Stephanie", "Jordan", 4, 3),
+("Jesse", "Finn", 5, 4),
+("Jason", "Bateman", 6, 5); 
