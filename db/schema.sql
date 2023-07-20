b131d6dafc3cf170d68d761de761a5c9a63c9509
sjf630 <95392688+sjf630@users.noreply.github.com>
2023-07-20 16:40:19 -0500

    fix schema errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
